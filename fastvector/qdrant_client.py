a76790d043a0c10b899eac57e4802ce77639e330
NirantK <nirant.bits@gmail.com>
2023-07-11 19:22:00 +0530

    wip

---
+++ fastvector/qdrant_client.py
@@ -0,0 +1,20 @@
+# qdrant_client.py
+
+
+class QdrantClient:
+    # Existing QdrantClient implementation...
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Check if fastvector is installed
+        try:
+            from fastvector.qdrant_mixin import QdrantClientMixin
+
+            # If it is, add the mixin methods to this instance
+            for name, method in QdrantClientMixin.__dict__.items():
+                if callable(method):
+                    setattr(self, name, method.__get__(self, self.__class__))
+        except ImportError:
+            # If it's not, do nothing
+            pass
