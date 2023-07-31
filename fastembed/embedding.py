36e276fa8b9059c346b5ca41d2f8f6a85ca3de82
NirantK <nirant.bits@gmail.com>
2023-07-31 16:27:39 +0530

    * feat(embedding.py): add support for tokenization and encoding of documents using the model * chore(pyproject.toml): add tokenizers as a dependency

--- fastembed/embedding.py
+++ fastembed/embedding.py
@@ -5,7 +5,10 @@ import tempfile
 from abc import ABC, abstractmethod
 from typing import Any, List
 
+import numpy as np
+import onnxruntime as ort
 import requests
+from tokenizers import Tokenizer
 from tqdm import tqdm
 
 
@@ -91,6 +94,37 @@ class DefaultEmbedding(Embedding):
         )
 
         model_dir = self.decompress_to_cache(targz_path=filepath)        
+        self.tokenizer = Tokenizer.from_file(f"{model_dir}/tokenizer.json")
+        self.tokenizer.enable_truncation(max_length=256)
+        self.tokenizer.enable_padding(pad_id=0, pad_token="[PAD]", length=256)
+        self.model = ort.InferenceSession(f"{model_dir}/model.onnx")
+
+    def encode(self, documents: List[str], batch_size: int = 32) -> List[np.ndarray]:
+        # Encode documents using the model
+        # https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+        
+        all_embeddings = []
+        for i in range(0, len(documents), batch_size):
+            batch = documents[i:i + batch_size]
+            encoded = [self.tokenizer.encode(d) for d in batch]
+            input_ids = np.array([e.ids for e in encoded])
+            attention_mask = np.array([e.attention_mask for e in encoded])
+            onnx_input = {
+                "input_ids": np.array(input_ids, dtype=np.int64),
+                "attention_mask": np.array(attention_mask, dtype=np.int64),
+                "token_type_ids": np.array([np.zeros(len(e), dtype=np.int64) for e in input_ids], dtype=np.int64),
+            }
+            model_output = self.model.run(None, onnx_input)
+            last_hidden_state = model_output[0]
+            # Perform mean pooling with attention weighting
+            input_mask_expanded = np.broadcast_to(np.expand_dims(attention_mask, -1), last_hidden_state.shape)
+            embeddings = np.sum(last_hidden_state * input_mask_expanded, 1) / np.clip(input_mask_expanded.sum(1), a_min=1e-9, a_max=None)
+            embeddings = normalize(embeddings).astype(np.float32)
+            all_embeddings.append(embeddings)
+        return np.concatenate(all_embeddings)
+
+
+
 
 
 class SentenceTransformersEmbedding(Embedding):
