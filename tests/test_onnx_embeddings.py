03b23af0a0a45b0116beef27d1b84f1c96dce681
NirantK <nirant.bits@gmail.com>
2023-08-25 00:37:31 +0530

    * refactor(test_onnx_embeddings.py): rename test_onnx_embeddigns.py to test_onnx_embeddings.py * chore(test_onnx_embeddings.py): remove unnecessary line breaks and whitespace * test(test_onnx_inference): add test case for onnx inference

--- tests/test_onnx_embeddings.py
+++ tests/test_onnx_embeddings.py
@@ -2,24 +2,14 @@ from fastembed.embedding import DefaultEmbedding
 
 
 def test_onnx_inference():
-    model = DefaultEmbedding(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
+    model = DefaultEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-    docs = [
-        "hello world"
-    ]
+    docs = ["hello world"]
 
-    expected_vector = [
-        -3.44772711e-02,  3.10231801e-02,  6.73499797e-03,  2.61089597e-02
-    ]
+    expected_vector = [-3.44772711e-02, 3.10231801e-02, 6.73499797e-03, 2.61089597e-02]
 
     vector = list(model.embed(docs))[0]
     assert len(vector) == 384
 
     for i in range(len(expected_vector)):
         assert abs(vector[i] - expected_vector[i]) < 1e-3
-
-
-
-
