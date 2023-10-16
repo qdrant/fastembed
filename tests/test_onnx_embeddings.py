c719fc696da780100b969c6a15c8efcb7be23827
generall <andrey@vasnetsov.com>
2023-10-16 13:27:28 +0200

    disable large models on non-ubuntu CI

--- tests/test_onnx_embeddings.py
+++ tests/test_onnx_embeddings.py
@@ -1,19 +1,25 @@
+import os
+
 import numpy as np
 from tqdm import tqdm
 
 from fastembed.embedding import DefaultEmbedding, Embedding
 
-
 CANONICAL_VECTOR_VALUES = {
-    "BAAI/bge-small-en": np.array([-0.0232, -0.0255,  0.0174, -0.0639, -0.0006]),
-    "BAAI/bge-base-en": np.array([0.0115,  0.0372,  0.0295,  0.0121,  0.0346]),
-    "sentence-transformers/all-MiniLM-L6-v2": np.array([0.0259,  0.0058,  0.0114,  0.0380, -0.0233]),
-    "intfloat/multilingual-e5-large": np.array([0.0098,  0.0045,  0.0066, -0.0354,  0.0070]),
+    "BAAI/bge-small-en": np.array([-0.0232, -0.0255, 0.0174, -0.0639, -0.0006]),
+    "BAAI/bge-base-en": np.array([0.0115, 0.0372, 0.0295, 0.0121, 0.0346]),
+    "sentence-transformers/all-MiniLM-L6-v2": np.array([0.0259, 0.0058, 0.0114, 0.0380, -0.0233]),
+    "intfloat/multilingual-e5-large": np.array([0.0098, 0.0045, 0.0066, -0.0354, 0.0070]),
 }
 
 
 def test_default_embedding():
+    is_ubuntu_ci = os.getenv("IS_UBUNTU_CI")
+
     for model_desc in Embedding.list_supported_models():
+        if is_ubuntu_ci == "false" and model_desc["model"] == "intfloat/multilingual-e5-large":
+            continue
+
         dim = model_desc["dim"]
         model = DefaultEmbedding(model_name=model_desc["model"])
 
