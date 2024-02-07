33f55962c1053abae85169a30f4c8101ba317868
Nirant Kasliwal <nirant.bits@gmail.com>
2024-02-07 22:22:41 +0530

    * fix(text_embedding_base.py): remove unnecessary prefix from texts in embed method * feat(text_embedding_base.py): update query_embed method to updated instruction for the v1.5 model

--- fastembed/text/text_embedding_base.py
+++ fastembed/text/text_embedding_base.py
@@ -37,7 +37,7 @@ class TextEmbeddingBase(ModelManagement):
         """
 
         # This is model-specific, so that different models can have specialized implementations
-        yield from self.embed([f"passage: {text}" for text in texts], **kwargs)
+        yield from self.embed(texts, **kwargs)
 
     def query_embed(self, query: Union[str, Iterable[str]], **kwargs) -> Iterable[np.ndarray]:
         """
@@ -52,6 +52,6 @@ class TextEmbeddingBase(ModelManagement):
 
         # This is model-specific, so that different models can have specialized implementations
         if isinstance(query, str):
-            yield from self.embed([f"query: {query}"], **kwargs)
+            yield from self.embed([f"Represent this sentence for searching relevant passages: {query}"], **kwargs)
         if isinstance(query, Iterable):
-            yield from self.embed([f"query: {text}" for text in query], **kwargs)
+            yield from self.embed([f"Represent this sentence for searching relevant passages:: {text}" for text in query], **kwargs)
