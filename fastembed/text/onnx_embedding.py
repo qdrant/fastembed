3c51019ff8f218bb3ed5b08bdf4275371d01ff8a
yuvraj wale <yuvrajwale16@gmail.com>
2024-03-20 23:42:05 +0530

    add: support mixedbread-ai/mxbai-embed-large-v1

--- fastembed/text/onnx_embedding.py
+++ fastembed/text/onnx_embedding.py
@@ -143,6 +143,15 @@ supported_onnx_models = [
     #         "https://storage.googleapis.com/qdrant-fastembed/sentence-transformers-all-MiniLM-L6-v2.tar.gz"
     #     ]
     # }
+    {
+    "model": "mixedbread-ai/mxbai-embed-large-v1",
+    "dim": 1024,
+    "description": "base sentence embedding model",
+    "size_in_GB": 1.34, 
+    "sources": {
+        "hf": "mixedbread-ai/mxbai-embed-large-v1",
+        },
+    },
 ]
 
 
