f2c5d69a7d172662c948498728570f6328c1c1bd
Yash <yashh.real@gmail.com>
2024-04-01 13:18:00 +0530

    updated

--- tests/test_text_onnx_embeddings.py
+++ tests/test_text_onnx_embeddings.py
@@ -7,55 +7,28 @@ from fastembed.text.text_embedding import TextEmbedding
 
 CANONICAL_VECTOR_VALUES = {
     "BAAI/bge-small-en": np.array([-0.0232, -0.0255, 0.0174, -0.0639, -0.0006]),
-    "BAAI/bge-small-en-v1.5": np.array(
-        [0.01522374, -0.02271799, 0.00860278, -0.07424029, 0.00386434]
-    ),
-    "BAAI/bge-small-zh-v1.5": np.array(
-        [-0.01023294, 0.07634465, 0.0691722, -0.04458365, -0.03160762]
-    ),
+    "BAAI/bge-small-en-v1.5": np.array([0.01522374, -0.02271799, 0.00860278, -0.07424029, 0.00386434]),
+    "BAAI/bge-small-zh-v1.5": np.array([-0.01023294, 0.07634465, 0.0691722, -0.04458365, -0.03160762]),
     "BAAI/bge-base-en": np.array([0.0115, 0.0372, 0.0295, 0.0121, 0.0346]),
-    "BAAI/bge-base-en-v1.5": np.array(
-        [0.01129394, 0.05493144, 0.02615099, 0.00328772, 0.02996045]
-    ),
-    "BAAI/bge-large-en-v1.5": np.array(
-        [0.03434538, 0.03316108, 0.02191251, -0.03713358, -0.01577825]
-    ),
-    "BAAI/bge-large-en-v1.5-quantized": np.array(
-        [0.03434538, 0.03316108, 0.02191251, -0.03713358, -0.01577825]
-    ),
-    "sentence-transformers/all-MiniLM-L6-v2": np.array(
-        [0.0259, 0.0058, 0.0114, 0.0380, -0.0233]
-    ),
-    "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2": np.array(
-        [0.0094, 0.0184, 0.0328, 0.0072, -0.0351]
-    ),
-    "intfloat/multilingual-e5-large": np.array(
-        [0.0098, 0.0045, 0.0066, -0.0354, 0.0070]
-    ),
+    "BAAI/bge-base-en-v1.5": np.array([0.01129394, 0.05493144, 0.02615099, 0.00328772, 0.02996045]),
+    "BAAI/bge-large-en-v1.5": np.array([0.03434538, 0.03316108, 0.02191251, -0.03713358, -0.01577825]),
+    "BAAI/bge-large-en-v1.5-quantized": np.array([0.03434538, 0.03316108, 0.02191251, -0.03713358, -0.01577825]),
+    "sentence-transformers/all-MiniLM-L6-v2": np.array([0.0259, 0.0058, 0.0114, 0.0380, -0.0233]),
+    "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2": np.array([0.0094, 0.0184, 0.0328, 0.0072, -0.0351]),
+    "intfloat/multilingual-e5-large": np.array([0.0098, 0.0045, 0.0066, -0.0354, 0.0070]),
     "sentence-transformers/paraphrase-multilingual-mpnet-base-v2": np.array(
         [-0.01341097, 0.0416553, -0.00480805, 0.02844842, 0.0505299]
     ),
-    "jinaai/jina-embeddings-v2-small-en": np.array(
-        [-0.0455, -0.0428, -0.0122, 0.0613, 0.0015]
-    ),
-    "jinaai/jina-embeddings-v2-base-en": np.array(
-        [-0.0332, -0.0509, 0.0287, -0.0043, -0.0077]
-    ),
-    "nomic-ai/nomic-embed-text-v1": np.array(
-        [0.0061, 0.0103, -0.0296, -0.0242, -0.0170]
-    ),
+    "jinaai/jina-embeddings-v2-small-en": np.array([-0.0455, -0.0428, -0.0122, 0.0613, 0.0015]),
+    "jinaai/jina-embeddings-v2-base-en": np.array([-0.0332, -0.0509, 0.0287, -0.0043, -0.0077]),
+    "nomic-ai/nomic-embed-text-v1": np.array([0.0061, 0.0103, -0.0296, -0.0242, -0.0170]),
     "nomic-ai/nomic-embed-text-v1.5": np.array(
         [-1.6531514e-02, 8.5380634e-05, -1.8171231e-01, -3.9333291e-03, 1.2763254e-02]
     ),
-    "thenlper/gte-large": np.array(
-        [-0.01920587, 0.00113156, -0.00708992, -0.00632304, -0.04025577]
-    ),
-    "mixedbread-ai/mxbai-embed-large-v1": np.array(
-        [0.02295546, 0.03196154, 0.016512, -0.04031524, -0.0219634]
-    ),
-    "intfloat/multilingual-e5-large-instruct": np.array(
-        [0.01020065, 0.0236722, 0.00117698, -0.04327101, 0.02887568]
-    ),
+    "thenlper/gte-large": np.array([-0.01920587, 0.00113156, -0.00708992, -0.00632304, -0.04025577]),
+    "mixedbread-ai/mxbai-embed-large-v1": np.array([0.02295546, 0.03196154, 0.016512, -0.04031524, -0.0219634]),
+    "intfloat/multilingual-e5-large-instruct": np.array([ 0.01020065,  0.0236722,   0.00117698, -0.04327101,  0.02887568]),
+
 }
 
 
@@ -75,14 +48,11 @@ def test_embedding():
         assert embeddings.shape == (2, dim)
 
         canonical_vector = CANONICAL_VECTOR_VALUES[model_desc["model"]]
-        assert np.allclose(
-            embeddings[0, : canonical_vector.shape[0]], canonical_vector, atol=1e-3
-        ), model_desc["model"]
+        assert np.allclose(embeddings[0, : canonical_vector.shape[0]], canonical_vector, atol=1e-3), model_desc["model"]
 
 
 @pytest.mark.parametrize(
-    "n_dims,model_name",
-    [(384, "BAAI/bge-small-en-v1.5"), (768, "jinaai/jina-embeddings-v2-base-en")],
+    "n_dims,model_name", [(384, "BAAI/bge-small-en-v1.5"), (768, "jinaai/jina-embeddings-v2-base-en")]
 )
 def test_batch_embedding(n_dims, model_name):
     model = TextEmbedding(model_name=model_name)
@@ -95,8 +65,7 @@ def test_batch_embedding(n_dims, model_name):
 
 
 @pytest.mark.parametrize(
-    "n_dims,model_name",
-    [(384, "BAAI/bge-small-en-v1.5"), (768, "jinaai/jina-embeddings-v2-base-en")],
+    "n_dims,model_name", [(384, "BAAI/bge-small-en-v1.5"), (768, "jinaai/jina-embeddings-v2-base-en")]
 )
 def test_parallel_processing(n_dims, model_name):
     model = TextEmbedding(model_name=model_name)
