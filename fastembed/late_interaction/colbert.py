fix: Fix broadcast issue

fastembed/late_interaction/colbert.py
@@ -70,18 +70,12 @@ class Colbert(LateInteractionTextEmbeddingBase, OnnxTextModel[np.ndarray]):
         self, onnx_input: Dict[str, np.ndarray], is_doc: bool = True
     ) -> Dict[str, np.ndarray]:
         original_length = onnx_input["input_ids"].shape[1]
+        marker_token = self.DOCUMENT_MARKER_TOKEN_ID if is_doc else self.QUERY_MARKER_TOKEN_ID
 
-        if is_doc:
-            onnx_input["input_ids"] = np.insert(
-                onnx_input["input_ids"], 1, self.DOCUMENT_MARKER_TOKEN_ID, axis=1
-            )
-        else:
-            onnx_input["input_ids"] = np.insert(
-                onnx_input["input_ids"], 1, self.QUERY_MARKER_TOKEN_ID, axis=1
-            )
-
+        onnx_input["input_ids"] = np.insert(onnx_input["input_ids"], 1, marker_token, axis=1)
         onnx_input["attention_mask"] = np.insert(onnx_input["attention_mask"], 1, 1, axis=1)
-        if onnx_input["input_ids"].shape[1] > original_length:
+
+        if not is_doc and onnx_input["input_ids"].shape[1] > original_length:
             onnx_input["input_ids"] = onnx_input["input_ids"][:, :original_length]
             onnx_input["attention_mask"] = onnx_input["attention_mask"][:, :original_length]
         return onnx_input