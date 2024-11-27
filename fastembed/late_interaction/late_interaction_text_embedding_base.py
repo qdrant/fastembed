e43f680a24c0015f19098bbb240911f06e9c6120
Dmitrii Rudenko <dmitrii.rudenko@qdrant.tech>
2024-11-27 10:51:14 +0100

    Refactoring to late interaction class

--- fastembed/late_interaction/late_interaction_text_embedding_base.py
+++ fastembed/late_interaction/late_interaction_text_embedding_base.py
@@ -42,9 +42,7 @@ class LateInteractionTextEmbeddingBase(ModelManagement):
         # This is model-specific, so that different models can have specialized implementations
         yield from self.embed(texts, **kwargs)
 
-    def query_embed(
-        self, query: Union[str, Iterable[str]], **kwargs
-    ) -> Iterable[np.ndarray]:
+    def query_embed(self, query: Union[str, Iterable[str]], **kwargs) -> Iterable[np.ndarray]:
         """
         Embeds queries
 
