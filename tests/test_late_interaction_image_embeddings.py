0ff8f49fe3c5bfd1511c64f54950f9ceb83e7086
Dmitrii Rudenko <dmitrii.rudenko@qdrant.tech>
2024-11-29 18:40:10 +0100

    Clean_up was a lie

--- tests/test_late_interaction_image_embeddings.py
+++ tests/test_late_interaction_image_embeddings.py
@@ -60,9 +60,6 @@ images = [
 
 def test_batch_embedding():
     is_ci = os.getenv("CI")
-    if is_ci:
-        delete_model_cache(model.model._model_dir)
-
     docs_to_embed = images
 
     for model_name, expected_result in CANONICAL_COLUMN_VALUES.items():
@@ -81,8 +78,6 @@ def test_batch_embedding():
 
 def test_single_embedding():
     is_ci = os.getenv("CI")
-    if is_ci:
-        delete_model_cache(model.model._model_dir)
 
     docs_to_embed = images
 
@@ -99,8 +94,6 @@ def test_single_embedding():
 
 def test_single_embedding_query():
     is_ci = os.getenv("CI")
-    if is_ci:
-        delete_model_cache(model.model._model_dir)
 
     queries_to_embed = queries
 
@@ -117,10 +110,9 @@ def test_single_embedding_query():
 
 def test_parallel_processing():
     is_ci = os.getenv("CI")
-    if is_ci:
-        delete_model_cache(model.model._model_dir)
 
     model = LateInteractionImageEmbedding(model_name="akshayballal/colpali-v1.2-merged")
+
     token_dim = 128
     docs = ["hello world", "flag embedding"] * 100
     embeddings = list(model.embed(docs, batch_size=10, parallel=2))
@@ -146,8 +138,6 @@ def test_parallel_processing():
 )
 def test_lazy_load(model_name):
     is_ci = os.getenv("CI")
-    if is_ci:
-        delete_model_cache(model.model._model_dir)
 
     model = LateInteractionImageEmbedding(model_name=model_name, lazy_load=True)
     assert not hasattr(model.model, "model")
