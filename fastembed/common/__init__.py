8f1364fcc25e3970afaa081a692f8c4de4f06ca1
George Panchuk <george.panchuk@qdrant.tech>
2025-02-04 18:34:13 +0100

    fix: remove redundant import

--- fastembed/common/__init__.py
+++ fastembed/common/__init__.py
@@ -1,3 +1,3 @@
-from fastembed.common.types import ImageInput, OnnxProvider, PathInput, PilInput
+from fastembed.common.types import ImageInput, OnnxProvider, PathInput
 
-__all__ = ["OnnxProvider", "ImageInput", "PathInput", "PilInput"]
+__all__ = ["OnnxProvider", "ImageInput", "PathInput"]
