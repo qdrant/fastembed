edb98211791bbee67c526953fcf60088318a5c67
hh-space-invader <h.hagag.ali@gmail.com>
2025-02-04 20:07:22 +0200

    chore: Updated stubs

--- tests/type_stub.py
+++ tests/type_stub.py
@@ -7,12 +7,20 @@ text_embedder = TextEmbedding(cache_dir="models")
 late_interaction_embedder = LateInteractionTextEmbedding(model_name="", cache_dir="models")
 reranker = TextCrossEncoder(model_name="", cache_dir="models")
 sparse_embedder = SparseTextEmbedding(model_name="", cache_dir="models")
-bm25_embedder = Bm25(model_name="", cache_dir="models")
+bm25_embedder = Bm25(
+    model_name="",
+    k=1.0,
+    b=1.0,
+    avg_len=1.0,
+    language="",
+    token_max_length=1,
+    disable_stemmer=False,
+    specific_model_path="models",
+)
 
 text_embedder.list_supported_models()
 text_embedder.embed(documents=[""], batch_size=1, parallel=1)
-text_embedder.embed(documents="", parallel=None)
-text_embedder.embed(documents="", batch_size=1, parallel=None, task_id=1)
+text_embedder.embed(documents="", parallel=None, task_id=1)
 text_embedder.query_embed(query=[""], batch_size=1, parallel=1)
 text_embedder.query_embed(query="", parallel=None)
 text_embedder.passage_embed(texts=[""], batch_size=1, parallel=1)
@@ -21,7 +29,6 @@ text_embedder.passage_embed(texts=[""], parallel=None)
 late_interaction_embedder.list_supported_models()
 late_interaction_embedder.embed(documents=[""], batch_size=1, parallel=1)
 late_interaction_embedder.embed(documents="", parallel=None)
-late_interaction_embedder.embed(documents="", batch_size=1, parallel=None)
 late_interaction_embedder.query_embed(query=[""], batch_size=1, parallel=1)
 late_interaction_embedder.query_embed(query="", parallel=None)
 late_interaction_embedder.passage_embed(texts=[""], batch_size=1, parallel=1)
@@ -36,7 +43,6 @@ reranker.rerank_pairs(pairs=[("", "")], parallel=None)
 sparse_embedder.list_supported_models()
 sparse_embedder.embed(documents=[""], batch_size=1, parallel=1)
 sparse_embedder.embed(documents="", batch_size=1, parallel=None)
-sparse_embedder.embed(documents="", batch_size=1, parallel=None)
 sparse_embedder.query_embed(query=[""], batch_size=1, parallel=1)
 sparse_embedder.query_embed(query="", batch_size=1, parallel=None)
 sparse_embedder.passage_embed(texts=[""], batch_size=1, parallel=1)
@@ -45,7 +51,6 @@ sparse_embedder.passage_embed(texts=[""], batch_size=1, parallel=None)
 bm25_embedder.list_supported_models()
 bm25_embedder.embed(documents=[""], batch_size=1, parallel=1)
 bm25_embedder.embed(documents="", batch_size=1, parallel=None)
-bm25_embedder.embed(documents="", batch_size=1, parallel=None, task_id=1)
 bm25_embedder.query_embed(query=[""], batch_size=1, parallel=1)
 bm25_embedder.query_embed(query="", batch_size=1, parallel=None)
 bm25_embedder.raw_embed(documents=[""])
