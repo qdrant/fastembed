f9d757ffc6e3e4fb15b93b9ecfde7c93e47c0cd1
George <george.panchuk@qdrant.tech>
2026-08-18 23:47:02 +0700

    fix: fix qwen (#680)

--- fastembed/common/utils.py
+++ fastembed/common/utils.py
@@ -33,8 +33,12 @@ def mean_pooling(input_array: NumpyArray, attention_mask: NDArray[np.int64]) -> 
 
 
 def last_token_pooling(input_array: NumpyArray, attention_mask: NDArray[np.int64]) -> NumpyArray:
-    """Take the embedding of the last non-padding token of each sequence."""
-    last_token_indices = np.maximum(attention_mask.sum(axis=1) - 1, 0)
+    """Take the embedding of the last non-padding token of each sequence.
+
+    Locates the last position the attention mask marks as real, so it holds whichever
+    side the tokenizer pads on.
+    """
+    last_token_indices = attention_mask.shape[1] - 1 - np.argmax(attention_mask[:, ::-1], axis=1)
     return input_array[np.arange(input_array.shape[0]), last_token_indices]
 
 
