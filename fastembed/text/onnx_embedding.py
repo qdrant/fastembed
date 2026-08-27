d8e4b494e977db870f9a4cbf4b70ff8f1f16db24
stephantul <stephantul@gmail.com>
2026-08-27 11:43:07 +0200

    amend description

--- fastembed/text/onnx_embedding.py
+++ fastembed/text/onnx_embedding.py
@@ -183,7 +183,7 @@ supported_onnx_models: list[DenseModelDescription] = [
     DenseModelDescription(
         model="minishlab/potion-base-8m",
         dim=256,
-        description=("Text embeddings, Unimodal (text), English, 2024 year."),
+        description=("Text embeddings, Extremely fast, Unimodal (text), English, 2024 year."),
         license="mit",
         size_in_GB=0.030,
         sources=ModelSource(hf="minishlab/potion-base-8m-onnx"),
@@ -192,7 +192,7 @@ supported_onnx_models: list[DenseModelDescription] = [
     DenseModelDescription(
         model="minishlab/potion-retrieval-32m",
         dim=512,
-        description=("Text embeddings, Unimodal (text), English, 2025 year."),
+        description=("Text embeddings, Extremely fast, Unimodal (text), English, 2025 year."),
         license="mit",
         size_in_GB=0.129,
         sources=ModelSource(hf="minishlab/potion-retrieval-32m-onnx"),
@@ -201,7 +201,7 @@ supported_onnx_models: list[DenseModelDescription] = [
     DenseModelDescription(
         model="minishlab/potion-multilingual-128m",
         dim=256,
-        description=("Text embeddings, Unimodal (text), Multilingual, 2025 year."),
+        description=("Text embeddings, Extremely fast, Unimodal (text), Multilingual, 2025 year."),
         license="mit",
         size_in_GB=0.512,
         sources=ModelSource(hf="minishlab/potion-multilingual-128m-onnx"),
